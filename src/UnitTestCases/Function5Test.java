09c59f9f8b589a4c03ece5c64da0987c5ca0e6c9
Aruna-Pala <42713572+Aruna-Pala@users.noreply.github.com>
2022-07-24 19:54:29 -0400

    modified code

--- src/UnitTestCases/Function5Test.java
+++ src/UnitTestCases/Function5Test.java
@@ -17,61 +17,61 @@ public class Function5Test {
 
 	/**
 	 * ID = TC1
-	 * Test Case = checking the input given by user using checkValid
-	 * Function Included = checkValid
+	 * Test Case = checking the input given by user using ValidationFunction
+	 * Function Included = ValidationFunction
 	 * Functional Requirement ID = FR1, FR2
 	 */
 	
 	@Test
     public void testCheckInput() {
         assertEquals("e", Function5.ValidationFunction("e"));
-        assertEquals("10", Function5.ValidationFunction("10"));
-        assertEquals("-12", Function5.ValidationFunction("-12"));
+        assertEquals("22", Function5.ValidationFunction("22"));
+        assertEquals("-23.4", Function5.ValidationFunction("-23.4"));
 	}
 	
 	/**
 	 * ID = TC2
-	 * Test  Case =  the calculation for magical number such as "e"
-	 * Function Included = exponential, function5or6
-	 * Functional Requirement ID = FR6
+	 * Test  Case =  the calculation for constant such as "e"
+	 * Function Included = exponential, Function5
+	 * Functional Requirement ID = FR5
 	 */
 	
 	@Test
     public void testMagicalNumbere() {
-        double sum = Function5.exponential(10);
+        double sum = Function5.exponential(2);
         String result = Double.toString(sum);
-        assertEquals(result, Function5.function5("1", "e", "10"));
+        assertEquals(result, Function5.function5("1", "e", "2"));
 	}
 	
 	/**
 	 * ID = TC3
 	 * Test  Case =  the calculation for positive and negative numbers
-	 * Function Included = function5or6
-	 * Functional Requirement ID = FR7, FR8
+	 * Function Included = function5
+	 * Functional Requirement ID = FR6,FR7
 	 */
 	@Test
-    public void testfunction5or6AsWhole() {
-        assertEquals("128.0", Function5.function5("2", "2", "6"));
-        assertEquals("-128.0", Function5.function5("-2", "2", "6"));
+    public void testfunction5() {
+        assertEquals("54.0", Function5.function5("2", "3", "3"));
+        assertEquals("-54.0", Function5.function5("-2", "3", "3"));
 	}
 	
 	/**
 	 * ID = TC4
 	 * Test  Case =  testing power function for the constant as base to the power x
-	 * Function Included = f5Power
-	 * Functional Requirement ID = FR3, FR8
+	 * Function Included = powerfunction
+	 * Functional Requirement ID = FR3, FR7
 	 */
 	@Test
     public void testNegativeF5PowerFunction() {
-        assertEquals(1024.0, Function5.powerfunction(-2,10),1);
-        assertEquals(-512.0, Function5.powerfunction(-2,9),1);
+        assertEquals(25.0, Function5.powerfunction(-5,2),1);
+        assertEquals(-19683.0, Function5.powerfunction(-3,9),1);
 	}
 	
 	/**
 	 * ID = TC5
 	 * Test  Case =  testing bigInteger for ab^x function
-	 * Function Included = function5or6
-	 * Functional Requirement ID = FR3, FR4
+	 * Function Included = function5
+	 * Functional Requirement ID = FR3
 	 */
 	@Test
     public void testBigIntegerPower() {
@@ -82,7 +82,7 @@ public class Function5Test {
 	/**
 	 * ID = TC6
 	 * Test  Case =  testing function with 0 as base and 0 in power
-	 * Function Included = function5or6
+	 * Function Included = function5
 	 * Functional Requirement ID = FR1
 	 */
 	@Test
