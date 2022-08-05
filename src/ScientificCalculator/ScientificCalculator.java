e831efac6d0ecb1b0b4d5e72844f0cf1bb97f3c2
Aruna-Pala <42713572+Aruna-Pala@users.noreply.github.com>
2022-08-04 23:50:20 -0400

    modified code

--- src/ScientificCalculator/ScientificCalculator.java
+++ src/ScientificCalculator/ScientificCalculator.java
@@ -10,24 +10,102 @@ public class ScientificCalculator {
 	 * also we used Algorithm 3 which is Divide and Conquer
 	 * approach where we are dividing our function 5 which ab^x as a * b^x
 	 */
-	public static double powerHandler(double b, int x) {
-		if (x == 0) {
-			return 1;
+
+	/**
+     * getting final powerHandler result for b^x
+	 * also we used Algorithm 3 which is Divide and Conquer
+	 * approach where we are dividing our function 5 which ab^x as a * b^x
+     * @param base b
+     * @param exponent x
+     * @return result of b^x
+     */
+	public static double powerHandler(double b, double x) {
+		double res = 1;
+
+		if (b % 1 == 0 && x % 1 == 0) {
+
+			int x_int = (int) Math.floor(Math.abs(x));
+			for (int i = 0; i < x_int; i++) {
+				res = res * b;
+			}
+			if (x < 0)
+				return 1 / res;
+			else
+				return res;
+		} else {
+			double logRes = getLog(b);
+			double exp = x * logRes;
+			res = exponent(exp) * 1;
+			return res;
 		}
-		if (x == 1) {
-			return b;
+
+	}
+
+	/**
+	 * getting log value for exp_base
+	 * 
+	 * @param exp_base b
+	 * @return log value
+	 */
+
+	public static double getLog(double b) {
+
+		double exp_res, exp_base, exp_left, exp_right, exp_temp, exp_value;
+		exp_value = 2.71828182845905;
+
+		exp_base = b;
+		exp_res = 0.0;
+		// This speeds up the convergence by calculating the integral
+		while (exp_base >= exp_value) {
+			exp_base /= exp_value;
+			exp_res++;
 		}
-		if (x % 2 == 0) {
-			return powerHandler(b * b, x / 2);
-		}
-		return b * powerHandler(b * b, x / 2);
+		exp_res += (exp_base / exp_value);
+		exp_base = b;
+		int counter = 0;
+		do {
+			counter++;
+			exp_temp = exp_res;
+			exp_left = (exp_base / (exponent(exp_res - 1.0)));
+			exp_right = ((exp_res - 1.0) * exp_value);
+			exp_res = ((exp_left + exp_right) / exp_value);
+			if (counter == 5)
+				break;
+		} while (exp_res != exp_temp);
+
+		return exp_res;
+
+	}
+
+	/**
+	 * getting exponent value for the given power
+	 * 
+	 * @param power p
+	 * @return exponent value
+	 */
+
+	public static double exponent(double p) {
+		double exp_temp, exp_pow, exp_frac, exp_intg, exp_logn;
+		exp_temp = p;
+		exp_frac = exp_temp;
+		exp_pow = (1.0 + exp_temp);
+		exp_intg = 1.0;
+
+		do {
+			exp_intg++;
+			exp_frac *= (exp_temp / exp_intg);
+			exp_logn = exp_pow;
+			exp_pow += exp_frac;
+		} while (exp_logn != exp_pow);
+
+		return exp_pow;
 	}
 
 	/*
 	 * Algorithm 1 which is Recursive approach where power of the function handled
 	 * recursively
 	 */
-	public static double powerFunction(double b, int x) {
+	public static double powerFunction(double b, double x) {
 		if (x < 0) {
 			return 1.0 / powerHandler(b, Math.abs(x));
 		}
@@ -44,7 +122,7 @@ public class ScientificCalculator {
 	public static String function5(String a, String b, String x) {
 		double y;
 		double temp_a = 0, temp_b = 0;
-		int temp_x = 0;
+		double temp_x = 0;
 
 		if (a.equals("e")) {
 			temp_a = 1;
@@ -61,7 +139,7 @@ public class ScientificCalculator {
 			if (x.equals("e")) {
 				temp_x = 1;
 			} else {
-				temp_x = Integer.parseInt(x);
+				temp_x = Double.parseDouble(x);
 			}
 
 			if (temp_b != 0.0) {
--- src/UnitTestCases/Function5Test.java
+++ src/UnitTestCases/Function5Test.java
@@ -5,11 +5,11 @@ import ScientificCalculator.ScientificCalculator;
 
 import org.junit.Test;
 
-
 /**
  * SOEN 6011 Test Cases for F5 = ab^x
+ * 
  * @author Aruna Devi Pala
- * Student Id: 40184469
+ *         Student Id: 40184469
  *
  */
 
@@ -21,74 +21,74 @@ public class Function5Test {
 	 * Function Included = ValidationFunction
 	 * Functional Requirement ID = FR1, FR2
 	 */
-	
+
 	@Test
-    public void testCheckInput() {
-        assertEquals("e", ScientificCalculator.ValidationFunction("e"));
-        assertEquals("22", ScientificCalculator.ValidationFunction("22"));
-        assertEquals("-23.4", ScientificCalculator.ValidationFunction("-23.4"));
+	public void testCheckInput() {
+		assertEquals("e", ScientificCalculator.ValidationFunction("e"));
+		assertEquals("22", ScientificCalculator.ValidationFunction("22"));
+		assertEquals("-23.4", ScientificCalculator.ValidationFunction("-23.4"));
 	}
-	
+
 	/**
 	 * ID = TC2
-	 * Test  Case =  the calculation for constant such as "e"
+	 * Test Case = the calculation for constant such as "e"
 	 * Function Included = exponential, Function5
 	 * Functional Requirement ID = FR5
 	 */
-	
+
 	@Test
-    public void testMagicalNumbere() {
-        double sum = ScientificCalculator.exponential(2);
-        String result = Double.toString(sum);
-        assertEquals(result, ScientificCalculator.function5("1", "e", "2"));
+	public void testMagicalNumbere() {
+		double sum = ScientificCalculator.exponential(2);
+		String result = Double.toString(sum);
+		assertEquals(result, ScientificCalculator.function5("1", "e", "2"));
 	}
-	
+
 	/**
 	 * ID = TC3
-	 * Test  Case =  the calculation for positive and negative numbers
+	 * Test Case = the calculation for positive and negative numbers
 	 * Function Included = function5
 	 * Functional Requirement ID = FR6,FR7
 	 */
 	@Test
-    public void testfunction5() {
-        assertEquals("54.0", ScientificCalculator.function5("2", "3", "3"));
-        assertEquals("-54.0", ScientificCalculator.function5("-2", "3", "3"));
+	public void testfunction5() {
+		assertEquals("54.0", ScientificCalculator.function5("2", "3", "3"));
+		assertEquals("-54.0", ScientificCalculator.function5("-2", "3", "3"));
 	}
-	
+
 	/**
 	 * ID = TC4
-	 * Test  Case =  testing power function for the constant as base to the power x
+	 * Test Case = testing power function for the constant as base to the power x
 	 * Function Included = powerFunction
 	 * Functional Requirement ID = FR3, FR7
 	 */
 	@Test
-    public void testNegativeF5powerFunction() {
-        assertEquals(25.0, ScientificCalculator.powerFunction(-5,2),1);
-        assertEquals(-19683.0, ScientificCalculator.powerFunction(-3,9),1);
+	public void testNegativeF5powerFunction() {
+		assertEquals(25.0, ScientificCalculator.powerFunction(-5, 2), 1);
+		assertEquals(-19683.0, ScientificCalculator.powerFunction(-3, 9), 1);
 	}
-	
+
 	/**
 	 * ID = TC5
-	 * Test  Case =  testing bigInteger for ab^x function
+	 * Test Case = testing bigInteger for ab^x function
 	 * Function Included = function5
 	 * Functional Requirement ID = FR3
 	 */
 	@Test
-    public void testBigIntegerPower() {
+	public void testBigIntegerPower() {
 		assertEquals("Infinity", ScientificCalculator.function5("20", "2", "6000"));
-        assertEquals("-Infinity", ScientificCalculator.function5("-2", "2", "10000"));
+		assertEquals("-Infinity", ScientificCalculator.function5("-2", "2", "10000"));
 	}
-	
+
 	/**
 	 * ID = TC6
-	 * Test  Case =  testing function with 0 as base and 0 in power
+	 * Test Case = testing function with 0 as base and 0 in power
 	 * Function Included = function5
 	 * Functional Requirement ID = FR1
 	 */
 	@Test
-    public void testAnythingPowerZero() {
+	public void testAnythingPowerZero() {
 		assertEquals("20.0", ScientificCalculator.function5("20", "2", "0"));
-        assertEquals("0.0", ScientificCalculator.function5("2", "0", "0"));
+		assertEquals("0.0", ScientificCalculator.function5("2", "0", "0"));
 	}
 
 }
